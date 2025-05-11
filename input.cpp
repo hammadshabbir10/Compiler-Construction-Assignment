2f591a16c0f8dc3f885ac80f204cf53947413738
Hammad Shabbir <hammadshabbir507@gmail.com>
2025-05-11 18:15:51 +0500

    Delete input.cpp

--- input.cpp
+++
@@ -1,8 +0,0 @@
-int main() {
-    int a = 5;
-    float b = 3.14; // valid declaration
-    $float invalid_var = 4.5; // error: '$' is invalid
-    /* multi-line
-       comment */
-    print(a);
-}
